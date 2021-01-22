872eee35f853a6507100fdbd5f38e8a7497107d8
VoronKor <ravorr@yandex.ru>
2021-01-22 13:47:51 +0300

    First commit

---
+++ src/main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.Scanner;
+import java.io.*;
+
+public class main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("hellow world");
+
+    }
+
+    
+}
+
+
+
